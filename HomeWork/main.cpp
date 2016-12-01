94f45938d0b8adf9f140c7933e8032380083fe6b
SnipGhost <snipghost@list.ru>
2016-12-01 11:12:54 +0400

    Added overload of '<<'

--- HomeWork/main.cpp
+++ HomeWork/main.cpp
@@ -9,6 +9,31 @@
 int main()
 {
 	system("chcp 1251 > nul");
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	// ПРОСТО ПРИМЕР!
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	Word *w = new Word;
+	w->len = 5;
+	w->symbols = new char[w->len];
+	w->symbols = "hell";
+
+	Sentence *s = new Sentence;
+	s->size = 3;
+	s->word = new Word*[s->size];
+	s->word[0] = w;
+	s->word[1] = w;
+	s->word[2] = w;
+
+	Text *t = new Text;
+	t->size = 2;
+	t->sent = new Sentence*[t->size];
+	t->sent[0] = s;
+	t->sent[1] = s;
+	cout << *t;
+
+	cout << t->sent[1]->word[2] << endl; 
+	// Без разыменования (есть оператор)
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	Text *text = getText(inputText(cin));
 	if (text != NULL) doMyHomeWork(text);
 	say("Завершение работы ...");
