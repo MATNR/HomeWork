d9dc562e791b10755e39b1079c653c721a00c12c
SnipGhost <snipghost@list.ru>
2016-12-04 22:27:46 +0400

    Some style fixes

--- HomeWork/interface.h
+++ HomeWork/interface.h
@@ -6,13 +6,21 @@
 #pragma once
 #ifndef INTERFACE_H
 #define INTERFACE_H
-
+//---------------------------------------------------------------------------
 #include <windows.h>
 #include <conio.h>
 #include <iostream>
 #include <dos.h>
-
+//---------------------------------------------------------------------------
+#define SMALL_SPACE ("                     ")
+#define LONG_SPACE ("                                ")
+//---------------------------------------------------------------------------
+const int WAIT_TIME = 5000;
+const int WAIT_TIME_TO_PRINT = 250;
+const int WAIT_TIME_TO_LOGO = 500;
+const int WAIT_TIME_TO_HIDE = 2000;
+//---------------------------------------------------------------------------
 int menuRun();
 void returnToMenu(); //Функция, которая возвращает в начальное меню
-
+//---------------------------------------------------------------------------
 #endif 
