7b2642dbea1bd94d326aae39610c0a7534a6cea8
SnipGhost <snipghost@list.ru>
2016-12-17 23:14:10 +0300

    Remake for interface

--- HomeWork/interface.h
+++ HomeWork/interface.h
@@ -13,15 +13,11 @@
 #include <dos.h>
 #include "packcage.h"
 //---------------------------------------------------------------------------
-#define SMALL_SPACE ("                     ")
-#define LONG_SPACE ("                                ")
-//---------------------------------------------------------------------------
 const int WAIT_TIME = 5000;
 const int WAIT_TIME_TO_PRINT = 250;
 const int WAIT_TIME_TO_LOGO = 500;
 const int WAIT_TIME_TO_HIDE = 2000;
 //---------------------------------------------------------------------------
 int menuRun(Text *&text);
-void returnToMenu(Text *&text); //Функция, которая возвращает в начальное меню
 //---------------------------------------------------------------------------
 #endif 
